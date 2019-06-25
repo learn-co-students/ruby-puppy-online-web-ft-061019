3e0d3a4992d0a8d098c6479de0318ab74317bdcf
jharrel <harrel.jason@gmail.com>
2019-06-24 22:42:42 -0700

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,19 @@
 class Dog
+    attr_accessor :name
 
-  @@all = []
-  attr_accessor :name
+    @@all = []
 
 
-  def initialize(name)
-    @@name = name
-    @@all << self
-  end
-
-  def self.all
-    @@all.each do |dog_name|
-      puts dog_name.name
+    def initialize(name)
+        @name = name
+        @@all << self
     end
-  end
 
-  def self.clear_all
-    @@all.clear
-  end
+    def self.all
+       puts @@all.map{ |dog| dog.name }
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
 end
